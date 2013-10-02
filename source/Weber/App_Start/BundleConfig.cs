30cce3c1cf13330d680385a90c564f0255bb5212
buddyngo <buddyngo@yahoo.com>
2013-10-02 01:17:35 -0700

    Initialize source

---
+++ source/Weber/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace myTutor
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.*"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui*"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"
+                        ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site*"
+                ));
+        }
+    }
+}
