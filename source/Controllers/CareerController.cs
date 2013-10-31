3bd2f2080626e150acab51799583827f147ece2c
buddyngo <buddyngo@yahoo.com>
2013-10-31 05:19:25 -0700

    update code

---
+++ source/Controllers/CareerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortal.Controllers
+{
+    public class CareerController : Controller
+    {
+        //
+        // GET: /Carrer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
